a47780fc083f90a2f284744c327058eeb74f8862
不会写代码的小白 <92523359+Abysmiler@users.noreply.github.com>
2023-08-22 15:14:16 +0800

    Update common.go

--- Go-Project/controller/common.go
+++ Go-Project/controller/common.go
@@ -1,5 +1,7 @@
 package controller
 
+import "gorm.io/gorm"
+
 type Response struct {
 	StatusCode int32  `json:"status_code"`
 	StatusMsg  string `json:"status_msg,omitempty"`
@@ -28,4 +30,26 @@ type User struct {
 	FollowCount   int64  `json:"follow_count,omitempty"`
 	FollowerCount int64  `json:"follower_count,omitempty"`
 	IsFollow      bool   `json:"is_follow,omitempty"`
+}
+
+type Message struct {
+	Id         int64  `json:"id,omitempty"`
+	Content    string `json:"content,omitempty"`
+	CreateTime string `json:"create_time,omitempty"`
+}
+
+type MessageSendEvent struct {
+	UserId     int64  `json:"user_id,omitempty"`
+	ToUserId   int64  `json:"to_user_id,omitempty"`
+	MsgContent string `json:"msg_content,omitempty"`
+}
+
+type MessagePushEvent struct {
+	FromUserId int64  `json:"user_id,omitempty"`
+	MsgContent string `json:"msg_content,omitempty"`
+}
+
+// RollbackTransaction 回滚事务
+func RollbackTransaction(tx *gorm.DB) {
+	tx.Rollback()
 }
