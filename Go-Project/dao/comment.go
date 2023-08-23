aa7a4b7a86122cf79068e2d843e48b9fe087b5a8
不会写代码的小白 <92523359+Abysmiler@users.noreply.github.com>
2023-08-23 11:56:36 +0800

    Update comment.go

--- Go-Project/dao/comment.go
+++ Go-Project/dao/comment.go
@@ -1,9 +1,14 @@
 package dao
 
+import (
+	"github.com/life-studied/douyin-simple/global"
+	"github.com/life-studied/douyin-simple/model"
+)
+
 // GetCommentByIdListById 根据video_id返回视频评论列表
-func GetCommentByIdListById(videoID int64) ([]Comment, error) {
-	var comments []Comment
-	err := SERVER_DB.Where("video_id = ?", videoID).Find(&comments).Error
+func GetCommentByIdListById(videoID int64) ([]model.Comment, error) {
+	var comments []model.Comment
+	err := global.DB.Where("video_id = ?", videoID).Find(&comments).Error
 	if err != nil {
 		return nil, err
 	}
@@ -13,7 +18,7 @@ func GetCommentByIdListById(videoID int64) ([]Comment, error) {
 // GetUserById 根据user_id返回用户结构体
 func GetUserById(userID int64) (User, error) {
 	var user User
-	err := SERVER_DB.Where("id = ?", userID).First(&user).Error
+	err := global.DB.Where("id = ?", userID).First(&user).Error
 	if err != nil {
 		return User{}, err
 	}
@@ -21,23 +26,23 @@ func GetUserById(userID int64) (User, error) {
 }
 
 // GetCommentById 通过commentID 返回comment结构体
-func GetCommentById(commentID int64) (Comment, error) {
-	var comment Comment
-	err := SERVER_DB.Where("id = ?", commentID).First(&comment).Error
+func GetCommentById(commentID int64) (model.Comment, error) {
+	var comment model.Comment
+	err := global.DB.Where("id = ?", commentID).First(&comment).Error
 	if err != nil {
-		return Comment{}, err
+		return model.Comment{}, err
 	}
 	return comment, nil
 }
 
 // CreateComment 创建评论
-func CreateComment(comment *Comment) error {
-	err := SERVER_DB.Create(comment).Error
+func CreateComment(comment *model.Comment) error {
+	err := global.DB.Create(comment).Error
 	return err
 }
 
 // DeleteCommentById 根据id删除评论
 func DeleteCommentById(commentID int64) error {
-	err := SERVER_DB.Where("id = ?", commentID).Delete(Comment{}).Error
+	err := global.DB.Where("id = ?", commentID).Delete(model.Comment{}).Error
 	return err
 }
